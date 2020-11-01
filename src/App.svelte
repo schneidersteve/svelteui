7748d3bd7b8e03b3a96004eac226a1f5fc38732a
schneidersteve <email@steveschneider.de>
2020-11-01 10:01:40 +0100

    Refactor base

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
-	export let name: string;
+import ButtonDemo from "./showcase/button/ButtonDemo.svelte";
+
+let currCount: number = 0;
+let currDoubled: number = 0;
+
+$: if (currCount >= 3) {
+		alert(`count is dangerously high!`);
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<ButtonDemo bind:count={currCount} bind:doubled={currDoubled}></ButtonDemo>
+	<p>{currCount} doubled is {currDoubled}</p>
 </main>
 
 <style>
@@ -13,18 +20,5 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
