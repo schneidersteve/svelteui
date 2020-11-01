453744c936de39d53d6c3a24e8edc2d22264edfe
schneidersteve <email@steveschneider.de>
2020-11-01 10:00:58 +0100

    Refactor base

---
+++ src/components/button/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let label: string;
+    export let icon: string;
+    export let iconPos: string = "left";
+    export let badge: string;
+    export let badgeClass: string;
+</script>
+
+<button class={classNames}></button>
---
+++ src/showcase/button/ButtonDemo.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let count: number = 0;
+
+    export let doubled: number = 0;
+    $: doubled = count * 2;
+
+    function handleClick(): void {
+        count += 1;
+    }
+</script>
+   
+<button on:click={handleClick}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
