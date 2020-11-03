4f84d626308b91f5f3373de8c8a4a5b010f65d1a
schneidersteve <email@steveschneider.de>
2020-11-03 14:10:22 +0100

    Added new stylesheet

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,5 @@
 <script lang="ts">
-import "primeicons/primeicons.css";
-
-import ButtonDemo from "./showcase/button/ButtonDemo.svelte";
-
-let currCount: number = 0;
-let currDoubled: number = 0;
-
-$: if (currCount >= 3) {
-		alert(`count is dangerously high!`);
-	}
+	import ButtonDemo from "./showcase/button/ButtonDemo.svelte";
 </script>
 
-<main>
-	<ButtonDemo bind:count={currCount} bind:doubled={currDoubled}></ButtonDemo>
-	<p>{currCount} doubled is {currDoubled}</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-</style>
+<ButtonDemo />
